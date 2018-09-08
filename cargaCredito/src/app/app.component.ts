d67531648e88adc2f17b4108fc1a1e84cc8f6e0d
Facundo Islas <FISLAS@prismamp.com>
2018-09-08 10:20:00 -0300

    Se agrega Carga Credito , splash, icono y login

--- cargaCredito/src/app/app.component.ts
+++ cargaCredito/src/app/app.component.ts
@@ -2,21 +2,36 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+import { LoginPage } from '../pages/login/login';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private auth: AuthProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+
+      this.auth.Session.subscribe(session=>{
+        if(session){
+          
+            this.rootPage = HomePage;
+        }
+          else{
+            this.rootPage= LoginPage ;
+          }
+      });
     });
+
+    
   }
 }
 
--- cargaCredito/src/app/app.module.ts
+++ cargaCredito/src/app/app.module.ts
@@ -6,25 +6,47 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthProvider } from '../providers/auth/auth';
+import { LoginPage } from '../pages/login/login';
+
+
+export const firebaseConfig = {
+ apiKey: "AIzaSyAAk8RAsSCmMaK-njE6glh5Xdvvg5Phaz8",
+    authDomain: "carga-de-credito-9b12a.firebaseapp.com",
+    databaseURL: "https://carga-de-credito-9b12a.firebaseio.com",
+    projectId: "carga-de-credito-9b12a",
+    storageBucket: "carga-de-credito-9b12a.appspot.com",
+    messagingSenderId: "421355991527"
+};
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider
   ]
 })
 export class AppModule {}
